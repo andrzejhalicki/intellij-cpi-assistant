7708ae1efd82b6f5f2ad7e95ac8a912b15c8601c
Andrzej <halicki.andrzej@gmail.com>
2025-08-29 11:25:57 +0200

    feat(): rrefresh artifact/download all scripts

---
+++ src/main/kotlin/com/cpiassistant/actions/GetResourceAction.kt
@@ -0,0 +1,66 @@
+package com.cpiassistant.actions
+
+import com.cpiassistant.nodes.CpiArtifact
+import com.cpiassistant.nodes.CpiResource
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.LocalFileSystem
+import org.jetbrains.annotations.NotNull
+import java.awt.event.InputEvent
+import java.util.*
+import javax.swing.JTree
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.TreePath
+
+class GetResourceAction : AnAction() {
+
+    override fun actionPerformed(@NotNull event: AnActionEvent) {
+        val project: Project? = event.project
+        val tree = event.getData(PlatformDataKeys.CONTEXT_COMPONENT) as JTree
+        val selectionPath: TreePath? = tree.selectionPath
+        val selectedNode = selectionPath?.lastPathComponent as? DefaultMutableTreeNode ?: return
+        val artifact = (selectedNode.parent as DefaultMutableTreeNode).userObject as CpiArtifact
+        val resource = selectedNode.userObject as CpiResource
+
+        if (resource.path.isEmpty()) {
+            val mapResourceAction: AnAction =
+                ActionManager.getInstance().getAction("com.cpiassistant.actions.MapResourceAction")
+            ActionManager.getInstance().tryToExecute(mapResourceAction, event.inputEvent,null,event.place,true)
+        } else {
+            val confirmResult = Messages.showYesNoDialog(
+                "Are you sure you want to overwrite local file?",
+                "Confirm Overwrite",
+                Messages.getQuestionIcon()
+            )
+
+            if (confirmResult != Messages.YES) {
+                return
+            }
+        }
+
+        artifact.downloadResource(resource.name) { content ->
+            val file = LocalFileSystem.getInstance().findFileByPath(resource.path)
+            if (file == null || !file.exists()) {
+                Messages.showInfoMessage("File does not exist", resource.path)
+                return@downloadResource
+            }
+            WriteCommandAction.runWriteCommandAction(project) {
+                file.setBinaryContent(content.toByteArray())
+            }
+            if (project != null) {
+                FileEditorManager.getInstance(project).openFile(file, true)
+            }
+        }
+
+    }
+
+}
---
+++ src/main/kotlin/com/cpiassistant/actions/RefreshArtifactAction.kt
@@ -0,0 +1,50 @@
+package com.cpiassistant.actions;
+
+import com.cpiassistant.nodes.CpiArtifact
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import org.jetbrains.annotations.NotNull
+import javax.swing.JTree
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeModel
+import javax.swing.tree.TreePath
+
+class RefreshArtifactAction : AnAction() {
+
+    override fun actionPerformed(@NotNull event: AnActionEvent) {
+        val tree = event.getData(PlatformDataKeys.CONTEXT_COMPONENT) as JTree
+        val selectionPath: TreePath? = tree.selectionPath
+        val selectedNode = selectionPath?.lastPathComponent as? DefaultMutableTreeNode ?: return
+        val artifact = selectedNode.userObject as CpiArtifact
+
+        artifact.isLoaded = false
+        artifact.refreshResources(artifact.id) { resources ->
+            if (resources.isEmpty()) {
+                artifact.isLoaded = true
+                return@refreshResources
+            }
+            selectedNode.removeAllChildren()
+            resources.forEach { resource ->
+                selectedNode.add(DefaultMutableTreeNode(resource))
+                resource.isLoaded = true
+            }
+            artifact.isLoaded = true
+            val model = tree.model as DefaultTreeModel
+            model.nodeStructureChanged(selectedNode)
+            tree.expandPath(selectionPath)
+
+            Notifications.Bus.notify(
+                Notification(
+                    "Custom Notification Group",
+                    "Artifact ${artifact.name} refreshed.",
+                    NotificationType.INFORMATION
+                )
+            )
+        }
+    }
+
+}
